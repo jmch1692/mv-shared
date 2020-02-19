14372d39f5255c584c8570bd89e37de7229d80bd
Valeria Zamora <valezamora@gmail.com>
2020-02-19 11:31:16 -0600

    MV-380 Nullable address fields (#15)

--- src/models/Address.ts
+++ src/models/Address.ts
@@ -1,4 +1,3 @@
-import { IsNotEmpty } from 'class-validator';
 import {
   Column,
   CreateDateColumn,
@@ -17,19 +16,15 @@ export class Address {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
 
-  @IsNotEmpty()
   @Column({ name: 'city' })
   public city: string;
 
-  @IsNotEmpty()
   @Column({ name: 'state' })
   public state: string;
 
-  @IsNotEmpty()
   @Column({ name: 'zipCode' })
   public zipCode: string;
 
-  @IsNotEmpty()
   @Column({ name: 'address1' })
   public address1: string;
 
