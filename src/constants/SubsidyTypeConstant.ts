f9012376137802ad863617671006f7c1e357b817
valeriazamora <valezamora@gmail.com>
2020-03-25 14:57:42 -0600

    MV-1095 Update name of Veterans Benefit

--- src/constants/SubsidyTypeConstant.ts
+++ src/constants/SubsidyTypeConstant.ts
@@ -1,6 +1,6 @@
 export enum SubsidyTypeEnum {
     CCCAP = 'CCCAP',
     BEST_BEGINNINGS = 'Best Beginnings',
-    VETERANS_BENEFIT = 'Veterans Benefit',
+    VETERANS_BENEFIT = "Veteran's Benefit",
     IN_PROCESS_OF_APPLYING = 'In the process of applying',
 }
