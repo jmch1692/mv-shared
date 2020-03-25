e1237ff136124f158a37a6a926ae3ea880f04a1a
PauloBarrantes <paulobarrantes98@gmail.com>
2020-03-25 15:46:42 -0600

    MV-1076  - :hammer: Added new slot types.

--- src/constants/SlotTypeConstant.ts
+++ src/constants/SlotTypeConstant.ts
@@ -5,4 +5,10 @@ export enum SlotTypeEnum {
   BONUS = 'Bonus',
   OVERLAP = 'Overlap',
   EXTENDED = 'Extended',
+  HALF_DAY_MORNING = 'Half Day - Morning',
+  HALF_DAY_AFTERNOON = 'Half Day - Afternoon',
+  SCHOOL_AGE = 'School-Age',
+  SHORT_TERM = 'Short Term',
+  EXTENDED_MORNING = 'Extended - Morning',
+  EXTENDED_AFTERNOON= 'Extended - Afternoon',
 }
